2fb76297e0634443c09e56e5490ac2eb1ff076bd
Parthifun <parthifun29@gmail.com>
2026-07-06 21:39:44 -0400

    Add temple package slider, hero redesign, UI improvements, and package page enhancements

--- static/js/packages.js
+++ static/js/packages.js
@@ -113,3 +113,27 @@ window.onclick = function(event) {
     }
 
 };
+
+/* ================================
+   HERO IMAGE SLIDER
+================================ */
+
+const slides = document.querySelectorAll(".slide");
+
+let currentSlide = 0;
+
+setInterval(() => {
+
+    slides[currentSlide].classList.remove("active");
+
+    currentSlide++;
+
+    if(currentSlide >= slides.length){
+
+        currentSlide = 0;
+
+    }
+
+    slides[currentSlide].classList.add("active");
+
+},5000);
