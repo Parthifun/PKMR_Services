ce11ae927c212dbceb30de202d99c8bf7802252a
Parthifun <parthifun29@gmail.com>
2026-07-20 18:54:18 -0400

    Add destination tour popup and new travel destinations

--- static/js/packages.js
+++ static/js/packages.js
@@ -1,4 +1,558 @@
 
+const destinationDetails = {
+
+    Arunachalam: {
+
+        image: "/static/images/Arunachalam.jpg",
+
+        subtitle: "The Linga of Fire",
+
+        places: [
+            "Arunachaleswarar Temple",
+            "Girivalam Path",
+            "Ramana Ashram",
+            "Skandashram",
+            "Virupaksha Cave",
+            "Seshadri Swamigal Ashram",
+            "Unnamulai Amman Temple",
+            "Annamalai Hill View",
+            "Indra Lingam",
+            "Adi Annamalai Temple"
+        ],
+
+        duration: "2 Days / 1 Night",
+
+        vehicles: [
+            "Sedan",
+            "SUV",
+            "Innova Crysta",
+            "Tempo Traveller"
+        ],
+
+        includes: [
+            "Fuel Charges",
+            "Toll Charges",
+            "Parking Charges",
+            "Driver Allowance"
+        ]
+
+    },
+
+    Annavaram: {
+
+        image: "/static/images/Annavaram.jpg",
+
+        subtitle: "Abode of Lord Veera Venkata Satyanarayana",
+
+        places: [
+            "Sri Satyanarayana Swamy Temple",
+            "Ratnagiri Hill",
+            "Temple View Point",
+            "Pampa River",
+            "Temple Gardens",
+            "Kalyana Mandapam",
+            "Prasadam Complex",
+            "Anna Prasadam Hall",
+            "Temple Museum",
+            "Nearby Coconut Groves"
+        ],
+
+        duration: "1 Day",
+
+        vehicles: [
+            "Sedan",
+            "SUV",
+            "Innova Crysta",
+            "Tempo Traveller"
+        ],
+
+        includes: [
+            "Fuel Charges",
+            "Toll Charges",
+            "Parking Charges",
+            "Driver Allowance"
+        ]
+
+    },
+
+    Coorg: {
+
+        image: "/static/images/Coorg.jpg",
+
+        subtitle: "Scotland of India",
+
+        places: [
+            "Abbey Falls",
+            "Raja's Seat",
+            "Dubare Elephant Camp",
+            "Talakaveri",
+            "Nisargadhama",
+            "Golden Temple",
+            "Mandalpatti Peak",
+            "Harangi Dam",
+            "Omkareshwara Temple",
+            "Chiklihole Reservoir"
+        ],
+
+        duration: "2 Days / 1 Night",
+
+        vehicles: [
+            "Sedan",
+            "SUV",
+            "Innova Crysta",
+            "Tempo Traveller"
+        ],
+
+        includes: [
+            "Fuel Charges",
+            "Toll Charges",
+            "Parking Charges",
+            "Driver Allowance"
+        ]
+
+    },
+
+    Kerala: {
+
+        image: "/static/images/Kerala.jpg",
+
+        subtitle: "God's Own Country",
+
+        places: [
+            "Munnar",
+            "Alleppey",
+            "Thekkady",
+            "Kovalam Beach",
+            "Athirappilly Falls",
+            "Varkala Beach",
+            "Kochi",
+            "Wayanad",
+            "Kumarakom",
+            "Bekal Fort"
+        ],
+
+        duration: "4 Days / 3 Nights",
+
+        vehicles: [
+            "Sedan",
+            "SUV",
+            "Innova Crysta",
+            "Tempo Traveller"
+        ],
+
+        includes: [
+            "Fuel Charges",
+            "Toll Charges",
+            "Parking Charges",
+            "Driver Allowance"
+        ]
+
+    },
+
+    Goa: {
+
+        image: "/static/images/Goa.jpg",
+
+        subtitle: "Pearl of the Orient",
+
+        places: [
+            "Baga Beach",
+            "Calangute Beach",
+            "Candolim Beach",
+            "Anjuna Beach",
+            "Vagator Beach",
+            "Fort Aguada",
+            "Chapora Fort",
+            "Dudhsagar Falls",
+            "Basilica of Bom Jesus",
+            "Dona Paula"
+        ],
+
+        duration: "4 Days / 3 Nights",
+
+        vehicles: [
+            "Sedan",
+            "SUV",
+            "Innova Crysta",
+            "Tempo Traveller"
+        ],
+
+        includes: [
+            "Fuel Charges",
+            "Toll Charges",
+            "Parking Charges",
+            "Driver Allowance"
+        ]
+
+    },
+
+    Bengaluru: {
+
+        image: "/static/images/Bengaluru.jpg",
+
+        subtitle: "Garden City of India",
+
+        places: [
+            "Lalbagh",
+            "Cubbon Park",
+            "Bangalore Palace",
+            "Vidhana Soudha",
+            "ISKCON Temple",
+            "Nandi Hills",
+            "Commercial Street",
+            "UB City",
+            "Bannerghatta Zoo",
+            "Wonderla"
+        ],
+
+        duration: "2 Days / 1 Night",
+
+        vehicles: [
+            "Sedan",
+            "SUV",
+            "Innova Crysta"
+        ],
+
+        includes: [
+            "Fuel Charges",
+            "Parking Charges",
+            "Toll Charges",
+            "Driver Allowance"
+        ]
+
+    },
+
+    Chennai: {
+
+        image: "/static/images/Chennai.jpg",
+
+        subtitle: "Gateway to South India",
+
+        places: [
+            "Marina Beach",
+            "Kapaleeshwarar Temple",
+            "Santhome Church",
+            "Fort St. George",
+            "Government Museum",
+            "Valluvar Kottam",
+            "Guindy Park",
+            "Mahabalipuram",
+            "Muttukadu",
+            "Elliot's Beach"
+        ],
+
+        duration: "2 Days / 1 Night",
+
+        vehicles: [
+            "Sedan",
+            "SUV",
+            "Innova Crysta"
+        ],
+
+        includes: [
+            "Fuel Charges",
+            "Parking Charges",
+            "Toll Charges",
+            "Driver Allowance"
+        ]
+
+    },
+
+    Hyderabad: {
+
+        image: "/static/images/Hyderabad.jpg",
+
+        subtitle: "City of Pearls",
+
+        places: [
+            "Charminar",
+            "Golconda Fort",
+            "Ramoji Film City",
+            "Salar Jung Museum",
+            "Hussain Sagar",
+            "Birla Temple",
+            "Chowmahalla Palace",
+            "Nehru Zoo",
+            "Tank Bund",
+            "Shilparamam"
+        ],
+
+        duration: "2 Days / 1 Night",
+
+        vehicles: [
+            "Sedan",
+            "SUV",
+            "Innova Crysta"
+        ],
+
+        includes: [
+            "Fuel Charges",
+            "Parking Charges",
+            "Toll Charges",
+            "Driver Allowance"
+        ]
+
+    },
+
+    Munnar: {
+
+        image: "/static/images/Munnar.jpg",
+
+        subtitle: "Kashmir of South India",
+
+        places: [
+            "Tea Gardens",
+            "Eravikulam National Park",
+            "Mattupetty Dam",
+            "Echo Point",
+            "Top Station",
+            "Kundala Lake",
+            "Tea Museum",
+            "Blossom International Park",
+            "Attukad Waterfalls",
+            "Pothamedu View Point"
+        ],
+
+        duration: "2 Days / 1 Night",
+
+        vehicles: [
+            "Sedan",
+            "SUV",
+            "Innova Crysta",
+            "Tempo Traveller"
+        ],
+
+        includes: [
+            "Fuel Charges",
+            "Toll Charges",
+            "Parking Charges",
+            "Driver Allowance"
+        ]
+
+    },
+
+    Mysore: {
+
+        image: "/static/images/Mysore.jpg",
+
+        subtitle: "City of Palaces",
+
+        places: [
+            "Mysore Palace",
+            "Chamundi Hills",
+            "St. Philomena's Church",
+            "Brindavan Gardens",
+            "Mysore Zoo",
+            "Karanji Lake",
+            "Rail Museum",
+            "Jaganmohan Palace",
+            "Lalitha Mahal",
+            "GRS Fantasy Park"
+        ],
+
+        duration: "2 Days / 1 Night",
+
+        vehicles: [
+            "Sedan",
+            "SUV",
+            "Innova Crysta"
+        ],
+
+        includes: [
+            "Fuel Charges",
+            "Parking Charges",
+            "Toll Charges",
+            "Driver Allowance"
+        ]
+
+    },
+
+    Ooty: {
+
+        image: "/static/images/Ooty.jpg",
+
+        subtitle: "Queen of Hill Stations",
+
+        places: [
+            "Botanical Garden",
+            "Ooty Lake",
+            "Doddabetta Peak",
+            "Rose Garden",
+            "Tea Museum",
+            "Pykara Lake",
+            "Pine Forest",
+            "Avalanche Lake",
+            "Emerald Lake",
+            "Nilgiri Mountain Railway"
+        ],
+
+        duration: "3 Days / 2 Nights",
+
+        vehicles: [
+            "SUV",
+            "Innova Crysta",
+            "Tempo Traveller"
+        ],
+
+        includes: [
+            "Fuel Charges",
+            "Parking Charges",
+            "Toll Charges",
+            "Driver Allowance"
+        ]
+
+    },
+
+    Pondicherry: {
+
+        image: "/static/images/Pondicherry.jpg",
+
+        subtitle: "The French Riviera of the East",
+
+        places: [
+            "Promenade Beach",
+            "Rock Beach",
+            "Auroville",
+            "Sri Aurobindo Ashram",
+            "Paradise Beach",
+            "French Colony",
+            "Botanical Garden",
+            "Chunnambar",
+            "Serenity Beach",
+            "Basilica of Sacred Heart"
+        ],
+
+        duration: "2 Days / 1 Night",
+
+        vehicles: [
+            "Sedan",
+            "SUV",
+            "Innova Crysta"
+        ],
+
+        includes: [
+            "Fuel Charges",
+            "Parking Charges",
+            "Toll Charges",
+            "Driver Allowance"
+        ]
+
+    },
+
+    Tirupati: {
+
+        image: "/static/images/Tirupati.jpg",
+
+        subtitle: "Spiritual Capital of Andhra Pradesh",
+
+        places: [
+            "Tirumala Temple",
+            "Sri Padmavathi Temple",
+            "Kapila Theertham",
+            "Sri Kalahasti",
+            "ISKCON Temple",
+            "Silathoranam",
+            "Akasa Ganga",
+            "Japali Hanuman Temple",
+            "Papavinasam",
+            "Chandragiri Fort"
+        ],
+
+        duration: "2 Days / 1 Night",
+
+        vehicles: [
+            "Sedan",
+            "SUV",
+            "Innova Crysta",
+            "Tempo Traveller"
+        ],
+
+        includes: [
+            "Fuel Charges",
+            "Parking Charges",
+            "Toll Charges",
+            "Driver Allowance"
+        ]
+
+    },
+
+    Thirumala: {
+
+        image: "/static/images/Thirumala.jpg",
+
+        subtitle: "The Abode of Lord Venkateswara",
+
+        places: [
+            "Sri Venkateswara Temple",
+            "Akasa Ganga",
+            "Papavinasam",
+            "Silathoranam",
+            "Japali Hanuman Temple",
+            "Srivari Padalu",
+            "Deer Park",
+            "Chakra Teertham",
+            "Rock Garden",
+            "TTD Gardens"
+        ],
+
+        duration: "1 Day",
+
+        vehicles: [
+            "Sedan",
+            "SUV",
+            "Innova Crysta",
+            "Tempo Traveller"
+        ],
+
+        includes: [
+            "Fuel Charges",
+            "Toll Charges",
+            "Parking Charges",
+            "Driver Allowance"
+        ]
+
+    },
+
+    Visakhapatnam: {
+
+        image: "/static/images/Visakhapatnam.jpg",
+
+        subtitle: "The Jewel of the East Coast",
+
+        places: [
+            "RK Beach",
+            "Kailasagiri",
+            "Yarada Beach",
+            "Rushikonda Beach",
+            "INS Kurusura Submarine Museum",
+            "TU 142 Aircraft Museum",
+            "Dolphin's Nose",
+            "Simhachalam Temple",
+            "Borra Caves",
+            "Araku Valley"
+        ],
+
+        duration: "3 Days / 2 Nights",
+
+        vehicles: [
+            "Sedan",
+            "SUV",
+            "Innova Crysta",
+            "Tempo Traveller"
+        ],
+
+        includes: [
+            "Fuel Charges",
+            "Toll Charges",
+            "Parking Charges",
+            "Driver Allowance"
+        ]
+
+    },
+
+};
+
+
 
 // ==========================================
 // PACKAGE MODAL
@@ -138,11 +692,11 @@ setInterval(() => {
 
 }, 5000);
 
-function openTourDetails(packageName){
+function openTourDetails(packageName) {
 
     const pkg = packageData[packageName];
 
-    if(!pkg) return;
+    if (!pkg) return;
 
     document.getElementById("tourTitle").innerHTML = pkg.title;
 
@@ -182,17 +736,17 @@ function openTourDetails(packageName){
 
 }
 
-function closeTourModal(){
+function closeTourModal() {
 
     document.getElementById("tourModal").style.display = "none";
 
 }
 
-window.addEventListener("click", function(event){
+window.addEventListener("click", function (event) {
 
     const modal = document.getElementById("tourModal");
 
-    if(event.target === modal){
+    if (event.target === modal) {
 
         closeTourModal();
 
@@ -229,3 +783,79 @@ document.addEventListener("DOMContentLoaded", () => {
     });
 
 });
+
+const popup = document.getElementById("tourPopup");
+
+const popupTitle = document.getElementById("popupTitle");
+const popupSubtitle = document.getElementById("popupSubtitle");
+const popupImage = document.getElementById("popupImage");
+const popupPlaces = document.getElementById("popupPlaces");
+const popupDuration = document.getElementById("popupDuration");
+const popupVehicles = document.getElementById("popupVehicles");
+const popupIncludes = document.getElementById("popupIncludes");
+
+document.querySelectorAll(".expand-btn").forEach(button => {
+
+    button.addEventListener("click", function () {
+
+        const card = this.closest(".tour-card");
+
+        const destination = card.dataset.destination;
+
+        const data = destinationDetails[destination];
+
+        if (!data) {
+
+            alert("Package details coming soon.");
+
+            return;
+
+        }
+
+        popupTitle.textContent = destination.toUpperCase();
+        popupSubtitle.textContent = data.subtitle;
+
+        popupImage.src = data.image;
+
+        popupPlaces.innerHTML = data.places
+            .map(place => `<li>${place}</li>`)
+            .join("");
+
+        popupDuration.textContent = data.duration;
+
+        popupVehicles.innerHTML = data.vehicles
+            .map(vehicle => `<li>${vehicle}</li>`)
+            .join("");
+
+        popupIncludes.innerHTML = data.includes
+            .map(item => `<li>${item}</li>`)
+            .join("");
+
+        popup.style.display = "flex";
+
+    });
+
+});
+
+document.querySelector(".tour-close").onclick = function () {
+
+    popup.style.display = "none";
+
+};
+
+document.querySelector(".popup-close-btn").onclick = function () {
+
+    popup.style.display = "none";
+
+};
+
+window.addEventListener("click", function (e) {
+
+    if (e.target === popup) {
+
+        popup.style.display = "none";
+
+    }
+
+});
+
